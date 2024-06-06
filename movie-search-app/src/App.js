ac7fcbaefc2c4f3788a82dc02b68c34ff328ac89
fairycreator <122693095+fairycreator@users.noreply.github.com>
2024-06-06 22:17:39 +0200

    app updated, set up routing and state management

--- movie-search-app/src/App.js
+++ movie-search-app/src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import axios from "axios";
+import SearchBar from "./components/SearchBar";
+import MovieList from "./components/MovieList";
+import MovieDetail from "./components/MovieDetail";
+import styled from "styled-components";
 
-function App() {
+const Container = styled.div`
+  max-width: 1200px;
+  margin: 0 auto;
+  padding: 20px;
+  text-align: center;
+`;
+
+const App = () => {
+  const [movies, setMovies] = useState([]);
+
+  const handleSearch = async (query) => {
+    const response = await axios.get(
+      `https://www.omdbapi.com/?s=${query}&apikey=YOUR_API_KEY`
+    );
+    setMovies(response.data.Search || []);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Container>
+        <h1>Movie Search App</h1>
+        <SearchBar onSearch={handleSearch} />
+        <Switch>
+          <Route path="/" exact>
+            <MovieList movies={movies} />
+          </Route>
+          <Route path="/movie/:id" component={MovieDetail} />
+        </Switch>
+      </Container>
+    </Router>
   );
-}
+};
 
 export default App;
